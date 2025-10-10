d96bd71d415870d8b0014fd13755ee99dc5d41f3
Isadora Vidor <isadora.vidor@icloud.com>
2025-10-09 22:16:58 -0300

    update solicitation routes

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from api.database_queries import create_solicitation
 
 # Run this file with:
 # uvicorn routes:app --reload
@@ -8,31 +9,35 @@ app = FastAPI()
 
 
 class SolicitationDTO(BaseModel):
-    id: int
-    date_time: str
-    is_approved: bool
-    reviewed: bool
+    solicitation_id: int
     vehicle_id: int
     user_id: int
 
 class TagDTO(BaseModel):
-    id: int
-    type: str
-    available: bool
-    register_date: str
-
+    tag_id: int
+    tag_type: str
 
 @app.post('/api/solicitations') 
 def add_solicitation(solicitation: SolicitationDTO) -> None:
-    pass
+    try:
+        create_solicitation(solicitation)
+        return {"message": "Solicitação criada com sucesso."}, 201
+    except ValueError as e:
+        print(f"Erro ao criar solicitação: {e}")
+        return {"error": str(e)}, 400
 
 @app.get('/api/solicitations')
 def get_solicitations() -> list[SolicitationDTO]:
     pass
     
-@app.patch('/api/solicitations/{id}')
-def update_solicitation_status(approved: bool) -> None:
-    pass
+@app.patch('/api/solicitations')
+def update_solicitation_status(solicitation_id: int, approved: bool) -> None:
+    try:
+        update_solicitation_status(solicitation_id, approved)
+        return {"message": "Status da solicitação atualizado com sucesso."}, 200
+    except ValueError as e:
+        print(f"Erro ao atualizar solicitação: {e}")
+        return {"error": str(e)}, 400
 
 @app.get('/api/tags')
 def get_tags() -> list[TagDTO]: 
