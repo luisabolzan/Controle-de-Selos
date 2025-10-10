f72aeadd6eccf24f72257d0149eb56d1d8b78998
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-10-09 23:50:01 -0300

    Added default response definitions, added get_solicitations endpoint

--- api/routes.py
+++ api/routes.py
@@ -1,33 +1,44 @@
 from fastapi import FastAPI
-from database_queries import create_solicitation
-from api_schemas import SolicitationDTO, TagDTO
+from database_queries import create_solicitation, get_all_solicitations, update_solicitation_status
+from api_schemas import SolicitationDTO, TagDTO,  ResponseDTO
 
 # Run this file with:
 # uvicorn routes:app --reload
 
 app = FastAPI()
 
+def success_response(data: any) -> ResponseDTO:
+    return ResponseDTO(success=True, data=data, message="success")
+
+def failed_response(message: str) -> ResponseDTO:
+    return ResponseDTO(success=False, message=message, data=None)
+
 @app.post('/api/solicitations') 
 def add_solicitation(solicitation: SolicitationDTO) -> None:
     try:
         create_solicitation(solicitation)
-        return {"message": "Solicitação criada com sucesso."}, 201
+        return success_response(message="Solicitação criada com sucesso.")
     except ValueError as e:
         print(f"Erro ao criar solicitação: {e}")
-        return {"error": str(e)}, 400
+        return failed_response(message=str(e))
 
 @app.get('/api/solicitations')
-def get_solicitations() -> list[SolicitationDTO]:
-    pass
-    
+def get_solicitations() -> ResponseDTO[list[SolicitationDTO]]:
+    try:
+        result = get_all_solicitations()
+        return success_response(data=result)
+    except ValueError as e:
+        print(f"Erro ao obter solicitações: {e}")
+        return failed_response(message=str(e))
+
 @app.patch('/api/solicitations')
-def update_solicitation_status(solicitation_id: int, approved: bool) -> None:
+def update_solicitation_status(solicitation_id: int, approved: bool) -> ResponseDTO[None]:
     try:
         update_solicitation_status(solicitation_id, approved)
-        return {"message": "Status da solicitação atualizado com sucesso."}, 200
+        return success_response(message="Status da solicitação atualizado com sucesso.")
     except ValueError as e:
         print(f"Erro ao atualizar solicitação: {e}")
-        return {"error": str(e)}, 400
+        return failed_response(message=str(e))
 
 @app.get('/api/tags')
 def get_tags() -> list[TagDTO]: 
