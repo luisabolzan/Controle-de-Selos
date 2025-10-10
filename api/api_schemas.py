24dd01838bcf3804d9f8ea31c45bb1fa14709a72
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-10-09 23:49:16 -0300

    Added default responseDTO

--- api/api_schemas.py
+++ api/api_schemas.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+from typing import Optional, TypeVar, Generic
+
+T = TypeVar('T')
+
+class ResponseDTO(BaseModel, Generic[T]):
+    success: bool
+    data: Optional[T] = None
+    message: Optional[str] = None
 
 class SolicitationDTO(BaseModel):
     vehicle_id: int
