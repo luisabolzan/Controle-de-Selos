1131e95793e4c0bfee5f4a0bf5126900fec5f262
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-10-09 21:54:33 -0300

    Added initial routes

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Run this file with:
 # uvicorn routes:app --reload
@@ -6,4 +7,37 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class SolicitationDTO(BaseModel):
+    id: int
+    date_time: str
+    is_approved: bool
+    reviewed: bool
+    vehicle_id: int
+    user_id: int
 
+class TagDTO(BaseModel):
+    id: int
+    type: str
+    available: bool
+    register_date: str
+
+
+@app.post('/api/solicitations') 
+def add_solicitation(solicitation: SolicitationDTO) -> None:
+    pass
+
+@app.get('/api/solicitations')
+def get_solicitations() -> list[SolicitationDTO]:
+    pass
+    
+@app.patch('/api/solicitations/{id}')
+def update_solicitation_status(approved: bool) -> None:
+    pass
+
+@app.get('/api/tags')
+def get_tags() -> list[TagDTO]: 
+    pass
+
+@app.post('/api/tags')
+def post_tag(tag: TagDTO) -> None:
+    pass
