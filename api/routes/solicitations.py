e8b177daaebac8e3262e43ca78fe376147aa5cca
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-11-24 15:25:42 -0300

    refactored service solicitation route

--- api/routes/solicitations.py
+++ api/routes/solicitations.py
@@ -1,11 +1,12 @@
-from api.api_schemas import ServiceTagSolicitationDTO, ResponseDTO, SolicitationDTO, SolicitationApproval
+from api.api_schemas import ServiceTagDTO, ServiceTagSolicitationDTO, ResponseDTO, SolicitationDTO, SolicitationApproval
 from api.database_queries import create_service_tag_solicitation, get_all_solicitations, set_solicitation_approval_status
 from api.database_models import Users
 from api.utils.security import get_current_user
+from api.database_access import get_db
 
 from fastapi import APIRouter, Depends
 
-solicitations_router = APIRouter(prefix='/solicitations')
+solicitations_router = APIRouter(prefix='/api/solicitations')
 
 def success_response(data: any) -> ResponseDTO:
     return ResponseDTO(success=True, data=data, message="success")
@@ -13,20 +14,25 @@ def success_response(data: any) -> ResponseDTO:
 def failed_response(message: str) -> ResponseDTO:
     return ResponseDTO(success=False, message=message, data=None)
 
-@solicitations_router.post('/api/solicitations/service')
+@solicitations_router.post('/service')
 def add_service_solicitation(
-    solicitation: ServiceTagSolicitationDTO,
+    solicitation_data: ServiceTagDTO,
+    session=Depends(get_db),
     current_user: Users = Depends(get_current_user)) -> ResponseDTO[None]:
-    print(solicitation)
     try:
-        solicitation.user_id = current_user.user_id
-        create_service_tag_solicitation(solicitation)
+        solicitation = ServiceTagSolicitationDTO(
+            user_id=current_user.user_id,
+            vehicle_id=solicitation_data.vehicle_id,
+            start_date=solicitation_data.start_date,
+            end_date=solicitation_data.end_date
+        )
+        create_service_tag_solicitation(solicitation, session)
         return success_response(data=None)
     except ValueError as e:
         print(f"Erro ao criar solicitação: {e}")
         return failed_response(message=str(e))
 
-@solicitations_router.get('/api/solicitations')
+@solicitations_router.get('/solicitations')
 def get_solicitations(current_user: Users = Depends(get_current_user)) -> ResponseDTO[list[SolicitationDTO]]:
     try:
         if not current_user.is_admin: 
