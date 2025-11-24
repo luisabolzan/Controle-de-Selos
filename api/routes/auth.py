e97690bc5533cbed735d1ba22182233616018b94
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-11-24 14:12:53 -0300

    Updated python requirements

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -2,29 +2,22 @@ from fastapi import HTTPException, Depends, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from api.database_queries import check_user_exists
-from api.api_schemas import TokenDTO
+from api.api_schemas import ResponseDTO
 from api.utils.security import verify_password, create_access_token
 
-auth_router = APIRouter(prefix='/auth')
+auth_router = APIRouter(prefix='/api/auth')
 
-@auth_router.post("/token", response_model=TokenDTO)
+@auth_router.post("/", response_model=TokenDTO)
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     # OAuth2PasswordRequestForm expects 'username' and 'password'
     user = check_user_exists(form_data.username)
     
-    if user is None:
+    if user is None or user.user_id is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User Not Found",
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User Not Found" if user is None else "Invalid Credentials",
             headers={"WWW-Authenticate": "Bearer"}
         )
     
-    elif not verify_password(form_data.password, user.password_hash):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalide Credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-        
     token = create_access_token(data=user.email)
-    return TokenDTO(token, "bearer")
+    return ResponseDTO(message="Login successful", success=True, data=None)
