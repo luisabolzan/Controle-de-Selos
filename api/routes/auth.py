45226be79bff616d379ef4b8c9b96c7bd8836dcd
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-11-24 15:26:16 -0300

    Added environment check and password verification

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,11 +1,16 @@
 from fastapi import HTTPException, Depends, Response, status, APIRouter, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+from os import getenv
 
 from api.database_queries import check_user_exists
 from api.api_schemas import ResponseDTO
 from api.utils.security import verify_password, create_access_token
 from api.database_access import get_db
+
+load_dotenv()
+env = getenv("ENV", "dev")
 
 auth_router = APIRouter(prefix='/api/auth')
 
@@ -14,10 +19,17 @@ def login(response:Response, form_data: OAuth2PasswordRequestForm = Depends(), s
     # OAuth2PasswordRequestForm expects 'username' and 'password'
     user = check_user_exists(form_data.username, session=session)
     
-    if user is None or user.user_id is None:
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User Not Found" if user is None else "Invalid Credentials",
+            detail="User Not Found",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    if not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Credentials",
             headers={"WWW-Authenticate": "Bearer"}
         )
     
@@ -26,7 +38,7 @@ def login(response:Response, form_data: OAuth2PasswordRequestForm = Depends(), s
         key="access_token",
         value=f"Bearer {token}",
         httponly=True,
-        secure=True, 
+        secure=env == "prod", 
         samesite="lax",
         max_age= 60 * 60 * 48 # 48h 
     )
